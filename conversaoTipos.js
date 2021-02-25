0d0ef5ba1339b7b6ae40d37bc6bc38dd801638c7
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-02-25 12:44:46 -0400

    conversao de tipo parseInt

---
+++ conversaoTipos.js
@@ -0,0 +1,9 @@
+console.log("Conversao de Tipos");
+
+console.log("ano "+ 2020);
+console.log("2"+"2");
+console.log(parseInt("2") + parseInt("2"));
+console.log("10" / "2");//conversao implicita, por tras do panos com a divisao
+console.log("reginaldo" / "2"); //Not a Number
+console.log(6.5); //ponto flutuante usa o ponto(.) 
+console.log(6,5);
