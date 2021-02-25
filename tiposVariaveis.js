c4d892a5e24f08d817e9408c04e0f6899d62ee95
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-02-25 10:11:05 -0400

    tipos de variaveis

---
+++ tiposVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Tipos de variaveis");
+// JS é case sensitive
+const idade = 20; //number
+const nome = 'reginaldo'; // string
+let ano = 2020 //number
+console.log(typeof(ano));
+console.log(typeof(idade));
+console.log(typeof(nome));
