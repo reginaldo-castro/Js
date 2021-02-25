216cf5fe12fcdf818fc7b4726e2920a8ae0f8ced
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-02-25 18:34:06 -0400

    Atribuicao de variaveis

---
+++ atribuicaoVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Atribuição de variaveis");
+const idade = 29;
+let nome= "reginaldo";
+const sobreNome = "Castro";
+
+//console.log(nome + "" + sobreNome);
+//console.log(nome, sobreNome );
+console.log(`Meu nome é ${nome} ${sobreNome}`);
+nome = nome + sobreNome;
+
+console.log(nome)
