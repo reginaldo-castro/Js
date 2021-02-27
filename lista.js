995cd98841177458d901239c0b0ab63d5fe2bf2e
reginaldo-castro <reginaldo.rc.castro@gmail.com>
2021-02-27 14:15:44 -0400

    Lista

---
+++ lista.js
@@ -0,0 +1,11 @@
+console.log("Lista em JS");
+const listaDeDestinos = new Array(
+  `Amazonas`,
+  `Acre`,
+  `Roraima`,
+  `São Paulo`,
+);
+
+listaDeDestinos.push(`Alagoas`); //adicionando um item na lista
+console.log("Destinos Possiveis");
+console.log(listaDeDestinos);
